da8a08bf91f26232a0de3d75b9922a2c1af37f05
Richo Healey <richo@psych0tik.net>
2014-03-22 23:31:35 +1100

    Ghetto command line parser

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::os::args;
+
+// const DEFAULT_PORT: int = 9000;
+static DEFAULT_PORT: int = 9000;
+
+fn usage() {
+    println!("Usage: weers address <port>");
+}
+
+fn main1(hostname: &str) {
+    run(hostname, DEFAULT_PORT);
+}
+
+fn main2(hostname: &str, port: &str) {
+    let oPort: Option<int> = from_str(port);
+    match oPort {
+        Some(p) => run(hostname, p),
+        None => usage(),
+    }
+}
+
+fn run(hostname: &str, port: int) {
+    println!("Ready to go on {}:{}", hostname, port);
+}
+
+fn main() {
+    let args = args();
+
+    match args.tail().len() {
+        1 => main1(args[1]),
+        2 => main2(args[1], args[2]),
+        _ => usage(),
+    }
+}
